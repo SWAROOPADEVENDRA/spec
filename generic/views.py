a9c171306f500b746727cb21efeade959e38b785
anudevendra <anushanossam@gmail.com>
2024-04-04 17:27:03 +0530

    first commit

---
+++ generic/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def anu(request):
+    return render(request,'anu.html')
+
+def nanna(request):
+    return render(request,'nanna.html')
